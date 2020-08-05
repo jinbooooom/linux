dec15ee1fd3aca90e22fae2d1eeab72dd71f0064
Jinbo <jinboom@foxmail.com>
2020-08-05 12:04:37 +0800

    add Question

--- programming/src/simplesem.c
+++ programming/src/simplesem.c
@@ -11,7 +11,7 @@
 // const int MAXSIZE = 10;
 int stack[MAXSIZE][2];
 int size = 0;
-sem_t sem;											// 其实 sem_t 就是长整型
+sem_t sem;						// 其实 sem_t 就是长整型
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;	// 编译时初始化锁为解锁状态
 
 void provide_data(void)
@@ -20,7 +20,7 @@ void provide_data(void)
 	{
 		stack[i][0] = i;
 		stack[i][1] = i;
-		sem_post(&sem);								// 信号量加 1 
+		sem_post(&sem);				// 信号量加 1 
 	}
 }
 
@@ -29,11 +29,11 @@ void handle_data1(void)
 	int i;
 	while (pthread_mutex_lock(&mutex),((i = size++) < MAXSIZE))	// 访问前先上锁
 	{
-		pthread_mutex_unlock(&mutex);							// 马上解锁？
-		sem_wait(&sem);											// 信号量减 1
+		pthread_mutex_unlock(&mutex);				// Q1：马上解锁？
+		sem_wait(&sem);						// 信号量减 1
 		printf("Plus: %d + %d = %d\n", stack[i][0], stack[i][1], stack[i][0] + stack[i][1]);
 	}
-	pthread_mutex_unlock(&mutex);								// 解两次锁？
+	pthread_mutex_unlock(&mutex);					// Q2：解两次锁？
 }
 
 
@@ -59,6 +59,31 @@ int main(int argc, char **argv)
 	pthread_join(thrd1, NULL);
 	pthread_join(thrd2, NULL);
 	pthread_join(thrd3, NULL);
-	sem_destroy(&sem);										// 删除信号量
+	sem_destroy(&sem);						// 删除信号量
 	return 0;
 }
+
+
+
+/* #####################################################################
+jinbo@fang:~/gitme/linux/programming/src$ gcc simplesem.c -lpthread -o simplesem.o
+jinbo@fang:~/gitme/linux/programming/src$ ./simplesem.o 
+Plus: 0 + 0 = 0
+Plus: 2 + 2 = 4
+Plus: 3 + 3 = 6
+Plus: 4 + 4 = 8
+Plus: 5 + 5 = 10
+Plus: 6 + 6 = 12
+Plus: 7 + 7 = 14
+Plus: 8 + 8 = 16
+Plus: 9 + 9 = 18
+Multiple: 1 * 1 = 2
+
+Question:
+运行结果跟书里完全一样，但有两处不理解：
+
+源代码 Q1 处: while 里上锁后为什么不执行关键区代码就马上解锁？
+源代码 Q2 处：如果 while 循环里解过一次锁，跳出 while 循环后，又要再解一次锁？
+
+*/
+
--- programming/src/thrdcreat.c
+++ programming/src/thrdcreat.c
@@ -18,10 +18,10 @@ int main(int argc, char **argv)
 	void *retval;
 	
 	ret = pthread_create(&thrd1, NULL, (void *)task1, (void *)&g1);
-	printf("I'm in main function\n");
+	printf("I'm in main function\n"); 		// 【Q1】
 	
 	ret = pthread_create(&thrd2, NULL, (void *)task2, (void *)&g2);
- 	cleanup(g1, g2);
+ 	cleanup(g1, g2);				// 【Q2】
 	getchar();
 
 	int tmp;	
@@ -75,6 +75,39 @@ void cleanup(int counter1, int counter2)
 	printf("total iterators: %d\n", counter1 + counter2);
 }
 
-// gcc thrdcreat.c -lpthread -o thrdcreat.o
+
+
+/* #####################################################################
+
+jinbo@fang:~/gitme/linux/programming/src$ gcc thrdcreat.c -lpthread -o thrdcreat.o
+jinbo@fang:~/gitme/linux/programming/src$ ./thrdcreat.o 
+I'm in main function   // Q1
+task1 count: 0
+task2 count: 0
+task2 count: 1
+task2 count: 2
+total iterators: 0     // Q2
+task2 count: 3
+task2 count: 4
+task1 count: 1
+task1 count: 2
+
+task1 count: 3
+task1 count: 4
+return val of task1 is 100
+total iterators: 10
+
+Question:
+运行结果跟书里不一样，体现在：
+
+源代码 Q1 处: 
+先创建线程 thrd1 不是应该先进入函数 task1 中，然后打印 task1 count: 0 吗？为什么先打印的是 I'm in main function?
+
+源代码 Q2 处: 
+第一次执行 cleanup(g1, g2); 打印 total iterators: 0，而之前已经打印了 task1 count: 0(（此时g1 = 1），task2 count: 2(此时g2 = 3)，
+那么执行完 cleanup(g1, g2) 应该等于 4，但运行结果却是 0？
+ 
+
+*/
 
 
