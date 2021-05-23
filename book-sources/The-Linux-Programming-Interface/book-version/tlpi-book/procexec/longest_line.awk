c17bc9046d95836cec72424c0b1ce4cdf85e0d59
Jinbo <jinboom@foxmail.com>
2021-05-23 09:37:24 +0800

    [refactor] 更改文件夹

--- book-sources/The-Linux-Programming-Interface/book-version/tlpi-book/procexec/longest_line.awk
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/awk -f
-length > max { max = length; }
-END 	     { print max; }
--- book-sources/The-Linux-Programming-Interface/book-version/tlpi-book/shlibs/Demo_static_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Build a program using a static library
-#
-
-cc -g -c mod1.c mod2.c mod3.c
-ar r libdemo.a mod1.o mod2.o mod3.o
-
-cc -g -o prog_static prog.c libdemo.a
-# Or: cc -g -o prog_static prog.c -L. -ldemo
