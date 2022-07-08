d5fca5d10a1e1bf05171830cc4f33099a1e820cc
Jinbo <jinboom@foxmail.com>
2022-07-08 17:10:05 +0800

    [shell][feat] Find the file ending in txt in the folder, rename it and package it.

--- command & shell/practise/001_按日期格式将磁盘使用情况写入文件.sh
+++ command & shell/practise/001_按日期格式将磁盘使用情况写入文件.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+## 题目
+
+# 请按照这样的日期格式（xxxx-xx-xx）每日生成一个文件，
+# 例如生成的文件名为2017-12-20.log， 
+# 并且把磁盘的使用情况写到到这个文件中。
+
 d=`date +%F` # 用 date --help 可以知道 %F 指完整日期格式，等价于 %Y-%m-%d
 dir=./tmp/disklog
 filename=${dir}/${d}.log
---
+++ command & shell/practise/005_找到文件夹下以txt结尾的文件重命名并打包.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+## 题目
+
+# 找到/123目录下所有后缀名为.txt的文件
+
+#   1. 批量修改.txt为.txt.bak
+#   2. 把所有.bak文件打包压缩为123.tar.gz
+#   3. 批量还原文件的名字，即把增加的.bak再删除
+
+# #### 核心要点
+
+# * find用来查找所有.txt文件
+# * tar打包一堆文件
+# * 还原文件名用for循环
+
+dir="./tmp/test"
+find $dir -type f -name "*.txt" > ${dir}/txt.list 
+for f in `cat ${dir}/txt.list`
+do
+    echo $f
+    mv $f $f.bak
+done
+
+#find /123/ -type f -name *.txt |xargs -i mv {} {}.bak 
+#find /123/ -type f -name *.txt -exec mv {} {}.bak \;
+
+for f in `cat ${dir}/txt.list`
+do
+    echo $f.bak
+done > ${dir}/txt.bak.list 
+
+tar -czvf ${dir}/005.tar.gz `cat ${dir}/txt.bak.list | xargs` # 与下面这一行相同
+#cat ${dir}/txt.bak.list | xargs tar -czvf ${dir}/005.tar.gz
+
+for f in `cat ${dir}/txt.list`
+do 
+    mv $f.bak $f
+done 
+
+
+# 清理工作
+tar -xzvf ${dir}/005.tar.gz # 也会打印
+
+sleep 5
+
+rm ${dir}/txt.list
+rm ${dir}/txt.bak.list
+find ${dir} -name "*.bak" | xargs rm
