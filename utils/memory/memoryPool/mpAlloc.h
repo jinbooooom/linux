23b209b361989058c1ae53c0dd7d2facf70a1f00
Jinbo <jinboom@foxmail.com>
2023-04-25 23:58:40 +0800

    feat(memory): add dynamic allocator

--- utils/memory/memoryPool/mpAlloc.h
+++ utils/memory/memoryPool/mpAlloc.h
@@ -2,8 +2,8 @@
  * @Author: jinboom
  * @Date: 2023-04-22 17:09:57
  * @LastEditors: jinboom
- * @LastEditTime: 2023-04-24 22:05:54
- * @FilePath: /linux/mnt/d/gitme/linux/utils/memory/memoryPool/mpAlloc.h
+ * @LastEditTime: 2023-04-25 23:57:13
+ * @FilePath: /linux/utils/memory/memoryPool/mpAlloc.h
  * @Description:
  *
  */
@@ -16,23 +16,21 @@
 
 typedef enum
 {
-    MEM_FIXED_BLOCK = 1,
+    MEM_FIXED_BLOCK   = 1,
     MEM_DYNAMIC_BLOCK = 4
 } mem_alloc_type_t;
 
 typedef int error_t;
 typedef char mem_header_t;
-typedef char *mem_tail_t;  // 存放扩容后的另一块内存区域基地址
+// typedef char mem_tail_t;  // 存放扩容后的另一块内存区域基地址
 
-struct MpAlloc final
+struct FixedAlloc final
 {
-    explicit MpAlloc(const size_t count, const size_t size, mem_alloc_type_t type);
-    explicit MpAlloc(const size_t count, const size_t size);
-    explicit MpAlloc(const size_t size);
-    MpAlloc()                           = delete;
-    MpAlloc(const MpAlloc &)            = delete;
-    MpAlloc &operator=(const MpAlloc &) = delete;
-    ~MpAlloc()                          = default;
+    explicit FixedAlloc(const size_t count, const size_t size);
+    FixedAlloc()                              = delete;
+    FixedAlloc(const FixedAlloc &)            = delete;
+    FixedAlloc &operator=(const FixedAlloc &) = delete;
+    ~FixedAlloc()                             = default;
 
     char *Malloc();
     error_t Free(char *vp);
@@ -40,15 +38,35 @@ struct MpAlloc final
     size_t mCount;
     size_t mBlockSize;
     size_t mTotalSize;
-    size_t mMemHeaderSize;
+    const size_t mMemHeaderSize;
     char *mBaseAddress;
     char *mEndAddress;  // 不包含尾部数据
-    
 
   private:
     bool isInvalidPointer(char *vp);
+};
 
+struct DynamicAlloc final
+{
+    explicit DynamicAlloc(const size_t size);
+    DynamicAlloc()                                = delete;
+    DynamicAlloc(const DynamicAlloc &)            = delete;
+    DynamicAlloc &operator=(const DynamicAlloc &) = delete;
+    ~DynamicAlloc()                               = default;
 
+    char *Malloc(uint32_t size);
+    error_t Free(char *vp);
+
+    size_t mTotalSize;
+    const size_t mMemHeaderSize;
+    const size_t mMemTailSize;  // used flag(1bit) + currentSize=0(3bit) + next address(8bit)
+    char *mBaseAddress;
+    char *mEndAddress;
+    char *mNextFitAddress;
+
+  private:
+    void setHeader(char *p, uint32_t size, uint8_t used);
+    bool isInvalidPointer(char *vp);
 };
 
 #endif
--- utils/memory/memoryPool/mptest.cpp
+++ utils/memory/memoryPool/mptest.cpp
@@ -2,24 +2,58 @@
  * @Author: jinboom
  * @Date: 2023-04-22 18:42:51
  * @LastEditors: jinboom
- * @LastEditTime: 2023-04-24 21:34:21
- * @FilePath: /linux/mnt/d/gitme/linux/utils/memory/memoryPool/mptest.cpp
+ * @LastEditTime: 2023-04-25 23:57:00
+ * @FilePath: /linux/utils/memory/memoryPool/mptest.cpp
  * @Description: memory pool test
  *
  */
 
 #include "memoryPool.h"
 
+#include <cassert>
+#include <map>
+#include <unordered_map>
+
+void header(char *p, uint32_t size)
+{
+    *(uint32_t *)p = size;
+    *(p + 3)       = 0x1;
+}
+
+void resetheader(char *p)
+{
+    *(p + 3) = 0x0;
+}
+
 int main()
 {
-    MemoryPool mp(18, 2);
+    int a   = 0x0;
+    char *p = (char *)&a;
+    char *q = p + 1;
+    char *r = q + 1;
+    char *s = r + 1;
+    *p      = 0x1;
+    *q      = 0x2;
+    *r      = 0x3;
+    *s      = 0x4;
 
+    printf("%p %p, %x\n", p, q, *(int *)p);  // 0x04030201
+    header(p, 5);
+    printf("%x\n", *(uint32_t *)p);
+    resetheader(p);
+    printf("%x\n", *(uint32_t *)p);
+
+    // assert(!(2 >> 1));
+
+    // return 0;
+
+    // MemoryPool mp(2, 17);
+    MemoryPool mp(128);
 
     char *p1 = mp.Malloc(3);
     char *p2 = mp.Malloc(7);
     char *p3 = mp.Malloc(15);
     char *p4 = mp.Malloc(17);
-
 
     logi("p1 = %p", p1);
     logi("p2 = %p", p2);
@@ -30,9 +64,6 @@ int main()
     p3 = nullptr;
     p3 = mp.Malloc(15);
     logi("p3 = %p", p3);
-
-    
-
 
     return 0;
 }
